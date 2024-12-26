b15051df903e6cf34485e07ceaa12e02963a81e5
Tolga Tektunalı <121040563+Tolga745@users.noreply.github.com>
2024-12-26 13:27:04 +0300

    Add files via upload

---
+++ cs405-project3/src/sceneNode.js
@@ -0,0 +1,49 @@
+/**
+ * @class SceneNode
+ * @desc A SceneNode is a node in the scene graph.
+ * @property {MeshDrawer} meshDrawer - The MeshDrawer object to draw
+ * @property {TRS} trs - The TRS object to transform the MeshDrawer
+ * @property {SceneNode} parent - The parent node
+ * @property {Array} children - The children nodes
+ */
+
+class SceneNode {
+    constructor(meshDrawer, trs, parent = null) {
+        this.meshDrawer = meshDrawer;
+        this.trs = trs;
+        this.parent = parent;
+        this.children = [];
+
+        if (parent) {
+            this.parent.__addChild(this);
+        }
+    }
+
+    __addChild(node) {
+        this.children.push(node);
+    }
+
+    draw(mvp, modelView, normalMatrix, modelMatrix) {
+        /**
+         * @Task1 : Implement the draw function for the SceneNode class.
+         */
+        
+        var transformedMatrix = this.trs.getTransformationMatrix();
+        var transformedModel = MatrixMult(modelMatrix, transformedMatrix);
+        var transformedModelView = MatrixMult(modelView, transformedMatrix);
+        var transformedMvp = MatrixMult(mvp, transformedMatrix);
+        var transformedNormals = MatrixMult(normalMatrix, transformedMatrix);
+
+        // Draw the MeshDrawer
+        if (this.meshDrawer) {
+            this.meshDrawer.draw(transformedMvp, transformedModelView, transformedNormals, transformedModel);
+        }
+
+        for (var i = 0; i < this.children.length; i++) {
+            this.children[i].draw(transformedMvp, transformedModelView, transformedNormals, transformedModel);
+        }
+    }
+
+    
+
+}
